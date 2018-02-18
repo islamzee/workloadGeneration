1f33f5e589856eb13471fd8043f717ab1cbe4f07
islamzee <zeenat@commlinkinfotech.com>
2018-02-18 13:09:13 +0600

    venv issue

---
+++ globals.py
@@ -0,0 +1,5 @@
+PATH_WIN = "C:\Users\a\Desktop\fragmented_task_usage"
+PATH_MAC = "/Users/zeenat/Desktop/fragmented_task_usage/*.csv"
+
+OUTPUT_PATH_WIN = "C:\Users\a\Desktop\workload.csv"
+OUTPUT_PATH_MAC = "/Users/zeenat/Desktop/workload.csv"
